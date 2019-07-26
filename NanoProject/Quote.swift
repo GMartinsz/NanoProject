00c5df63f71ccb8cddbd6355d1388d882493609b
GMartinz <gmdalosto@gmail.com>
2019-07-26 17:15:14 -0300

    aa

---
+++ NanoProject/Quote.swift
@@ -0,0 +1,20 @@
+//
+//  Quote.swift
+//  NanoProject
+//
+//  Created by Guilherme Martins Dalosto de Oliveira on 26/07/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+class Quote{
+    
+    
+    
+    
+    
+    
+    
+}
